4edc97a04bf4c5507747aebec13834ff82127ba8
anjaly13 <toanjalysnair@gmail.com>
2020-10-31 13:42:40 +0530

    Initial commit

---
+++ src/main/java/com/teamplayer/docanalyzer/DocAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.teamplayer.docanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamplayer/docanalyzer/DocAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamplayer.docanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
